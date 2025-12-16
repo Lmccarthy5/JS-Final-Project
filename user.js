985373f4a980af9142f280cb5a8040afc9586db9
Lmccarthy5 <mccarthylynn23@gmail.com>
2025-12-15 18:07:42 -0800

    third commit

--- user.js
+++ user.js
@@ -2,22 +2,20 @@ async function main() {
     const shows = await fetch(`https://api.jikan.moe/v4/seasons/2023/spring?sfw`);
     const showsData = await shows.json();
     const animeListEl = document.querySelector(`.anime_list`);
-
-    animeListEl.innerHTML = showsData
-    console.log(
-    showsData.data.map(data =>
-    ` <div class="show-card">
+console.log(showsData)
+    animeListEl.innerHTML = showsData.data
+    .map(show =>` 
+        <div class="show-card">
             <div class="show-card__container">
-              <h3>Title</h4>
-              <h3>English Title<h4>
-                <p><b>Year</b></p>
-                <p><b>Rated</b></p>
-                <p><b>Released</b></p>
+              <h3><b>Title:<b>${title}</h3>
+              <h4><b>English Title:<b>${title_english}<h4>
+                <p><b>Year:</b>${year}</p>
+                <p><b>Rating:</b>${rating}</p>
+                <p><b>Synopsis:</b>${synopsis}</p>
             </div>
-          </div>`
-        )
-        .join("")
-    )
+          </div>
+          `)
+        .join("");
 }
 
 main();
