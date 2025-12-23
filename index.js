50f1fdb8f1446e2d21ae7078ee32506d3a585574
Lmccarthy5 <mccarthylynn23@gmail.com>
2025-12-22 17:35:41 -0800

    Redone

--- index.js
+++
@@ -1,7 +0,0 @@
-//fetch(`https://api.jikan.moe/v4/seasons/2023/spring?sfw`)
-
-const indexInput = document.getElementById(".index__input")
-indexInput.addEventListener('keyup' , e => {
-    let currentValue = e.target.value;
-    console.log(currentValue)
-})
--- user.js
+++ user.js
@@ -1,26 +1,28 @@
-async function main() {
-    const shows = await fetch(`https://api.jikan.moe/v4/seasons/2023/spring?sfw`);
-    const showsData = await shows.json();
-    const animeListEl = document.querySelector(`.anime-list`);
 
-    animeListEl.innerHTML = showsData.data
+
+function searchChange(event) {
+    console.log(event.target.value)
+}
+
+
+async function renderMovies() {
+    const shows = await fetch(`https://www.omdbapi.com/?apikey=23ac2af0&s=jurassic`);
+    const showsData = await shows.json();
+    const movieListEl = document.querySelector(`.movie-list`);
+
+    movieListEl.innerHTML = showsData.Search
     .map(show =>` 
         <div class="show-card" onclick="${show.id}">
             <div class="show-card__container">
               <h3><b>Title:<b> ${show.title}</h3>
-              <h4><b>English Title:<b> ${show.title_english}<h4>
                 <p><b>Year:</b> ${show.year}</p>
-                <p><b>Rating:</b> ${show.rating}</p>
-                <p><b>Synopsis:</b> ${show.synopsis}</p>
+                <img href="${show.poster}"
             </div>
           </div>
           `)
         .join("");
+        console.log(showsData)
 }
 
-main();
+renderMovies();
 
-function showImg(id) {
-    localStorage.getItem("img", id);
-    window.location.href = `${window.location.id}`
-}
